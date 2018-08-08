05a6de9de4f03b8686b2983b6f384ca608102570
Daparrag <daparrag@correo.udistrital.edu.co>
2018-08-08 10:28:46 +0200

    Including Cube Class

--- app/src/main/java/opengl_app1/javacodegeeks/com/opengl_app1/Cube.java
+++ app/src/main/java/opengl_app1/javacodegeeks/com/opengl_app1/Cube.java
@@ -19,6 +19,7 @@ public class Cube {
     private ShortBuffer indexBuffer;
     private final int mProgram;
     private int mPositionHandle;
+    private int mColorHandle;
     private final int BYTES_PER_ELEMENT_FLOAT = 4;
     private final int BYTES_PER_ELEMENT_SHORT = 2;
 
@@ -27,49 +28,49 @@ public class Cube {
 
             //X,Y,Z 			    R,G,B
             // Top
-             -1.0f, 1.0f, -1.0f,   1.0f, 1.0f, 0.0f,
-             -1.0f, 1.0f, 1.0f,    0.7f, 0.5f, 1.0f,
-             1.0f, 1.0f, 1.0f,     0.1f, 0.5f, 0.5f,
-             1.0f, 1.0f, -1.0f,    0.5f, 1.0f, 0.6f,
+            -1.0f, 1.0f, -1.0f,   1.0f, 1.0f, 0.0f,1.0f,
+            -1.0f, 1.0f, 1.0f,    0.7f, 0.5f, 1.0f,1.0f,
+            1.0f, 1.0f, 1.0f,     0.1f, 0.5f, 0.5f,1.0f,
+            1.0f, 1.0f, -1.0f,    0.5f, 1.0f, 0.6f,1.0f,
 
 
             // Left
-            -1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f,
-            -1.0f, -1.0f, 1.0f,   0.7f, 0.5f, 1.0f,
-            -1.0f, -1.0f, -1.0f,  0.1f, 0.5f, 0.5f,
-            -1.0f, 1.0f, -1.0f,   0.5f, 1.0f, 0.6f,
+            -1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f,1.0f,
+            -1.0f, -1.0f, 1.0f,   0.7f, 0.5f, 1.0f,1.0f,
+            -1.0f, -1.0f, -1.0f,  0.1f, 0.5f, 0.5f,1.0f,
+            -1.0f, 1.0f, -1.0f,   0.5f, 1.0f, 0.6f,1.0f,
 
 
             // Right
-            1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f,
-            1.0f, -1.0f, 1.0f,   0.7f, 0.5f, 1.0f,
-            1.0f, -1.0f, -1.0f,  0.1f, 0.5f, 0.5f,
-            1.0f, 1.0f, -1.0f,   0.5f, 1.0f, 0.6f,
+            1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f,1.0f,
+            1.0f, -1.0f, 1.0f,   0.7f, 0.5f, 1.0f,1.0f,
+            1.0f, -1.0f, -1.0f,  0.1f, 0.5f, 0.5f,1.0f,
+            1.0f, 1.0f, -1.0f,   0.5f, 1.0f, 0.6f,1.0f,
 
             // Front
-            1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f,
-            1.0f, -1.0f, 1.0f,    0.7f, 0.5f, 1.0f,
-           -1.0f, -1.0f, 1.0f,    0.1f, 0.5f, 0.5f,
-           -1.0f, 1.0f, 1.0f,    0.5f, 1.0f, 0.6f,
+            1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f,1.0f,
+            1.0f, -1.0f, 1.0f,    0.7f, 0.5f, 1.0f,1.0f,
+            -1.0f, -1.0f, 1.0f,    0.1f, 0.5f, 0.5f,1.0f,
+            -1.0f, 1.0f, 1.0f,    0.5f, 1.0f, 0.6f,1.0f,
 
             // Back
-            1.0f, 1.0f, -1.0f,    1.0f, 1.0f, 0.0f,
-            1.0f, -1.0f, -1.0f,    0.7f, 0.5f, 1.0f,
-           -1.0f, -1.0f, -1.0f,    0.1f, 0.5f, 0.5f,
-           -1.0f, 1.0f, -1.0f,    0.5f, 1.0f, 0.6f,
+            1.0f, 1.0f, -1.0f,    1.0f, 1.0f, 0.0f,1.0f,
+            1.0f, -1.0f, -1.0f,    0.7f, 0.5f, 1.0f,1.0f,
+            -1.0f, -1.0f, -1.0f,    0.1f, 0.5f, 0.5f,1.0f,
+            -1.0f, 1.0f, -1.0f,    0.5f, 1.0f, 0.6f,1.0f,
 
             // Bottom
-            -1.0f, -1.0f, -1.0f,   1.0f, 1.0f, 0.0f,
-            -1.0f, -1.0f, 1.0f,    0.7f, 0.5f, 1.0f,
-            1.0f, -1.0f, 1.0f,     0.1f, 0.5f, 0.5f,
-            1.0f, -1.0f, -1.0f,    0.5f, 1.0f, 0.6f
+            -1.0f, -1.0f, -1.0f,   1.0f, 1.0f, 0.0f,1.0f,
+            -1.0f, -1.0f, 1.0f,    0.7f, 0.5f, 1.0f,1.0f,
+            1.0f, -1.0f, 1.0f,     0.1f, 0.5f, 0.5f,1.0f,
+            1.0f, -1.0f, -1.0f,    0.5f, 1.0f, 0.6f,1.0f,
 
     };//array not yet used by the graphics card
 
 
     /*cube indices*/
 
-    final short cubeindices[] =
+    final  short cubeindices[] =
             {
                     //indicate which set of vertices form the triangles
                     // form each face of the cube
@@ -102,13 +103,39 @@ public class Cube {
             };
 
 
+//    private final String vertexShaderCode =
+//            "attribute vec4 vertPosition;     \n"     // Per-vertex position information we will pass in.
+//                    + "attribute vec4 vertColor;        \n"     // Per-vertex color information we will pass in.
+//
+//                    + "varying vec4 v_Color;          \n"     // This will be passed into the fragment shader.
+//
+//                    + "void main()                    \n"     // The entry point for our vertex shader.
+//                    + "{                              \n"
+//                    + "   v_Color = vertColor;          \n"     // Pass the color through to the fragment shader.
+//                    // It will be interpolated across the triangle.
+//                    + "   gl_Position = vertPosition;\n"     // gl_Position is a special variable used to store the final position.
+//                    + "\n"     // Multiply the vertex by the matrix to get the final point in
+//                    + "}                              \n";    // normalized screen coordinates.
+//
+//    private final String fragmentShaderCode =
+//
+//            "precision mediump float;       \n"     // Set the default precision to medium. We don't need as high of a
+//                    // precision in the fragment shader.
+//                    + "varying vec4 v_Color;          \n"     // This is the color from the vertex shader interpolated across the
+//                    // triangle per fragment.
+//                    + "void main()                    \n"     // The entry point for our fragment shader.
+//                    + "{                              \n"
+//                    + "   gl_FragColor = v_Color;     \n"     // Pass the color directly through the pipeline.
+//                    + "}                              \n";
+//
+
 
     private final String vertexShaderCode =
             "precision mediump float;       \n"
             +""
             +"attribute vec3 vertPosition;  \n"
-            +"attribute vec3 vertColor;     \n"
-            +"varying vec3 fragColor;       \n"
+            +"attribute vec4 vertColor;     \n"
+            +"varying vec4 fragColor;       \n"
             +"\n"
             +"uniform mat4 mWorld;          \n"
             +"uniform mat4 mView;           \n"
@@ -116,7 +143,8 @@ public class Cube {
             +"void main ()                  \n"
             +"{\n"
             +"fragColor = vertColor;        \n"
-            +"gl_Position = mProj * mView * mWorld *  vec4(vertPosition, 1.0);\n"
+            +"gl_Position = mWorld * vec4(vertPosition, 1.0);\n"
+            + "\n"
             +"\n"
             +"}                             \n";
 
@@ -124,10 +152,10 @@ public class Cube {
     private final String fragmentShaderCode =
             "precision mediump float;       \n"
             +"\n"
-            +"varying vec3 fragColor;       \n"
+            +"varying vec4 fragColor;       \n"
             +"void main()                   \n"
             +"{                             \n"
-            +"gl_FragColor = vec4(fragColor, 1.0);\n"
+            +"gl_FragColor = fragColor;\n"
             +"}                             \n";
 
 
@@ -149,7 +177,6 @@ public class Cube {
         // Bind the vertex shader to the program.
         GLES20.glBindAttribLocation(mProgram,0,"vertPosition");
         GLES20.glBindAttribLocation(mProgram,1,"vertColor");
-
         GLES20.glLinkProgram(mProgram);                  // create OpenGL program executables
 
         // allocate cube indices buffer
@@ -163,28 +190,56 @@ public class Cube {
                 .order(ByteOrder.nativeOrder()).asShortBuffer();
         indexBuffer.put(cubeindices).position(0);
 
+
+        //Bin buffer and data
+        //GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER,0 ,indexBuffer,GLES20.GL_STATIC_DRAW);
+
+
+
+
     }
 
 
 
-    public void draw(float[] mvpMatrix){
+    public void draw(float[] mMVPMatrix){
 
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vertPosition");
-
+        vertexBuffer.position(0);
         GLES20.glVertexAttribPointer(
                 mPositionHandle,
                 3,
                 GLES20.GL_FLOAT,
                 false,
-                6*BYTES_PER_ELEMENT_FLOAT,
+                7*BYTES_PER_ELEMENT_FLOAT,
                 vertexBuffer
         );
 
         GLES20.glEnableVertexAttribArray(mPositionHandle);
 
+        // get handle to fragment shader's vColor member
+        mColorHandle = GLES20.glGetAttribLocation(mProgram, "vertColor");
+        vertexBuffer.position(3); //buffer offset
+        GLES20.glVertexAttribPointer(
+                mColorHandle,
+                4,
+                GLES20.GL_FLOAT,
+                false,
+                7*BYTES_PER_ELEMENT_FLOAT,
+                vertexBuffer
+        );
+
+        GLES20.glEnableVertexAttribArray(mColorHandle);
+        GLES20.glUseProgram(mProgram);
 
 
 
+
+        int mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram,"mWorld");
+
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+                GLES20.glDrawElements(GLES20.GL_TRIANGLES,cubeindices.length,GLES20.GL_UNSIGNED_SHORT,indexBuffer);
+                //GLES20.glDisableVertexAttribArray(mPositionHandle);
+                //GLES20.glDisableVertexAttribArray(mColorHandle);
 
     }
 
--- app/src/main/java/opengl_app1/javacodegeeks/com/opengl_app1/MyGLRenderer.java
+++ app/src/main/java/opengl_app1/javacodegeeks/com/opengl_app1/MyGLRenderer.java
@@ -7,12 +7,19 @@ import android.opengl.Matrix;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.os.SystemClock;
+
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
     private Triangle mTriangle;
     private Cube mCube;
     // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float [] wordMatrix = new float[16];
+    private final float [] viewMatrix = new float[16];
+    private final float [] projMatrix = new float[16];
+
+
     private final float[] mMVPMatrix = new float[16];
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
@@ -24,18 +31,53 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+        // Create a new perspective projection matrix. The height will stay the same
+        // while the width will vary as per aspect ratio.
+        final float ratio = (float) width / height;
+        final float left = -ratio;
+        final float right = ratio;
+        final float bottom = -1.0f;
+        final float top = 1.0f;
+        final float near = 4.0f;
+        final float far = 10.0f;
 
-        float ratio = (float) width / height;
-
-        // this projection matrix is applied to object coordinates
-        // in the onDrawFrame() method
-        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+        Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
         GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        mTriangle = new Triangle();
+        //mTriangle = new Triangle();
+
+
+        // Position the eye in front of the origin.
+        final float eyeX = 5.0f;
+        final float eyeY = 5.0f;
+        final float eyeZ = -0.5f;
+
+        // We are looking toward the distance
+        final float lookX = 0.0f;
+        final float lookY = 0.0f;
+        final float lookZ = -5.0f;
+
+        // Set our up vector. This is where our head would be pointing were we holding the camera.
+        final float upX = 0.0f;
+        final float upY = 1.0f;
+        final float upZ = 0.0f;
+
+        // Set the view matrix. This matrix can be said to represent the camera position.
+        // NOTE: In OpenGL 1, a ModelView matrix is used, which is a combination of a model and
+        // view matrix. In OpenGL 2, we can keep track of these matrices separately if we choose
+
+
+        Matrix.setLookAtM(viewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
+
+
+
+
+
+
+
         mCube = new Cube();
     }
 
@@ -43,27 +85,29 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl10) {
         float[] scratch = new float[16];
-
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
 
-        // Set the camera position (View matrix)
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
-
-        // Calculate the projection and view transformation
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
-
-
-        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, 1.0f);
-
-        // Combine the rotation matrix with the projection and camera view
-        // Note that the mMVPMatrix factor *must be first* in order
-        // for the matrix multiplication product to be correct.
-        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+        // Do a complete rotation every 10 seconds.
+        long time = SystemClock.uptimeMillis() % 10000L;
+        float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
+        // Set the identity Matrix
+        Matrix.setIdentityM(wordMatrix,0);
+        Matrix.translateM(wordMatrix, 0, 0.0f, 0.0f, -5.0f);
+        Matrix.rotateM(wordMatrix, 0, angleInDegrees, 0.0f, 1.0f, 0.0f);
 
 
-        // Draw triangle
-        mTriangle.draw(scratch);
+        // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
+        // (which currently contains model * view).
+        Matrix.multiplyMM(mMVPMatrix, 0, viewMatrix, 0, wordMatrix, 0);
+
+        // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
+        // (which now contains model * view * projection).
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
+
+
+
+        mCube.draw(mMVPMatrix);
     }
 
     public static int loadShader(int type, String shaderCode){
